96d1080954ef5678f022aa644e81d973e531da03
Zakaria AIT ERRAMI <zakaria.ait.errami00@gmail.com>
2022-08-15 00:56:17 +0100

    Adding cart Reducer (useContext) - Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,25 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const entredAmount = amountInputRef.current.value
+    const entredAmountNumber = +entredAmount;
+    if(entredAmount.trim().length === 0 || entredAmountNumber < 1 || entredAmountNumber>5){
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(entredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -12,10 +27,11 @@ const MealItemForm = (props) => {
           min: "1",
           max: "5",
           step: "1",
-          defaultValue: "1"
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
